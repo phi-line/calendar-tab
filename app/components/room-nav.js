92bfaa73fc8934b049d122587b7e18b50df6fd36
phi- <phi.liney@gmail.com>
2017-07-12 20:04:27 -0700

    added new styling for the room selector

--- app/components/room-nav.js
+++ app/components/room-nav.js
@@ -1,15 +1,5 @@
 import Ember from 'ember';
-import config from '../config/environment';
-const {Component, computed} = Ember;
+const {Component} = Ember;
 
 export default Component.extend({
-  rooms: config.rooms,
-  currentRoom: 'The Shire',
-
-  actions: {
-    updateRoom: function(room){
-      this.set('currentRoom', room);
-      this.sendAction('updateRoom', room);
-    }
-  }
 });
---
+++ app/components/room-selector.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+import config from '../config/environment';
+const {Component} = Ember;
+
+export default Component.extend({
+  rooms: config.rooms,
+  currentRoom: 'The Shire',
+
+  actions: {
+    updateRoom: function(room){
+      this.set('currentRoom', room);
+      this.sendAction('updateRoom', room);
+    }
+  }
+});
