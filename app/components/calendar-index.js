8eee9d7fb526971d24e078e770641cf151d029aa
phi- <phi.liney@gmail.com>
2017-07-12 18:08:15 -0700

    added basic structure for actions passed from the room-nav to main index

---
+++ app/components/calendar-index.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+const {Component} = Ember;
+
+export default Component.extend({
+  init() {
+    this._super();
+    this.set('room', 'The Shire');
+  },
+
+  room: null,
+
+  actions: {
+    updateRoom: function(room) {
+      alert(room);
+      this.set('room', room);
+    }
+  }
+});
