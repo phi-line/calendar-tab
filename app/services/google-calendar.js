ee0f7c5cccfab8763d74d6af5ed787138384ce9d
phi- <phi.liney@gmail.com>
2017-07-13 14:02:01 -0700

    removed my google calendar servive. now we use torii

--- app/services/google-calendar.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-import config from '../config/environment';
-
-export default Ember.Service.extend({
-  CLIENT_ID: config.key,
-  DISCOVERY_DOCS: ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"],
-  SCOPES: "https://www.googleapis.com/auth/calendar.readonly"
-
-});
