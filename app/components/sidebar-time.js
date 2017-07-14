c189c89252e5cd32188d85b743e19c9ab4e03443
Jonathan Pe <jonathanpe@admins-MacBook-Pro-2.local>
2017-07-14 10:44:26 -0700

    added sidebar-time formatting

--- app/components/sidebar-time.js
+++ app/components/sidebar-time.js
@@ -1,5 +1,9 @@
 import Ember from 'ember';
 const {Component} = Ember;
 
+var curTime = new Date();
+
 export default Component.extend({
+  time: curTime.getHours() + " : " + curTime.getMinutes(),
+  date: (curTime.getMonth() + 1) + "/" + curTime.getDate() + "/" + curTime.getFullYear()
 });
