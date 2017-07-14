d925439119923c07aba559d4b4fc6bd28f7c60de
phi- <phi.liney@gmail.com>
2017-07-14 10:43:58 -0700

    did some style changes to sidebar

--- app/components/event-item.js
+++ app/components/event-item.js
@@ -1,5 +1,16 @@
 import Ember from 'ember';
-const {Component} = Ember;
+import moment from 'moment';
+const {Component, computed} = Ember;
 
 export default Component.extend({
+  eventName: computed('event.summary', function(){
+    return this.get('event.summary');
+  }),
+
+  eventTime: computed('event.start.dateTime', 'event.end.dateTime', function() {
+    let start = moment(this.get('event.start.dateTime'));
+    let end = moment(this.get('event.end.dateTime'));
+    let format = (start.format('h:mm') + ' - ' + end.format('h:mm'));
+    return format;
+  })
 });
