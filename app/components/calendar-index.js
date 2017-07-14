bce940319bc7c2593b6b152331fe2e75ea3e4cc9
phi- <phi.liney@gmail.com>
2017-07-14 07:39:58 -0700

    we have propogation

--- app/components/calendar-index.js
+++ app/components/calendar-index.js
@@ -2,31 +2,51 @@ import Ember from 'ember';
 const {A, Component, computed, computed: {readOnly}, inject: { service }, observer, on} = Ember;
 
 export default Component.extend({
-  init() {
-    this._super();
-    this.set('room', 'The Shire');
-  },
-
   room: null,
-  events: A([{name: 'Event A'}]),
+  events: A([]),
+  calendars: A([]),
+
+  currentEvent: null,
 
   session: service('session'),
   isAuth: readOnly('session.isAuthenticated'),
-  calendars: on('init', computed('isAuth', function() {
+  getRooms: on('init', observer('isAuth', function() {
+    const self = this;
     if (this.get('isAuth')) {
       gapi.client.calendar.calendarList.list({
       }).then(function(response) {
         let calendars = response.result.items;
         calendars = calendars.filter(function(cal) {
           return cal.summary.includes('(Room)');
-        })
-        return calendars;
+        });
+        console.log(calendars);
+        self.set('calendars', calendars);
       });
     }
   })),
 
-  onRoomChange: observer('room', function(){
 
+  getRoomEvents: observer('room', function(){
+    const self = this;
+    if (self.get('calendars').length > 0){
+      let roomCalendar = self.get('calendars').filter(function(cal) {
+        return cal.summary.includes(self.get('room'));
+      });
+      if (roomCalendar) {
+        gapi.client.calendar.events.list({
+          'calendarId': roomCalendar[0].id,
+          'timeMin': (new Date()).toISOString(),
+          'showDeleted': false,
+          'singleEvents': true,
+          'maxResults': 5,
+          'orderBy': 'startTime'
+          }).then(function(response) {
+            let events = response.result.items;
+            console.log(events);
+            self.set('events', events);
+          })
+      }
+    }
   }),
 
   actions: {
